b33c36025323d2d9200f72f1c819ff799a4fe0df
mikewu0511 <wxh19850501@126.com>
2014-11-27 17:07:02 +0800

    added to support python version < 2.7, otherwise timedelta has total_seconds()

--- src/api/util/timeutils.py
+++ src/api/util/timeutils.py
@@ -2,5 +2,7 @@ from datetime import datetime
 
 def convert_to_epoch(timestamp):
 	diff = (timestamp - datetime(1970, 1, 1))
-	seconds = int(diff.total_seconds())
+	# added to support python version < 2.7, otherwise timedelta has total_seconds()
+	differenceTotalSeconds = (diff.microseconds + (diff.seconds + diff.days*24*3600) * 1e6) / 1e6
+	seconds = int(differenceTotalSeconds)
 	return seconds
